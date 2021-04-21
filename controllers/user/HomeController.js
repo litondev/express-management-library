15f79a045f9e932cb4b2ce6394dca6efcd434038
toniwidodo123 <litoninot12345@gmail.com>
2021-04-21 08:14:49 -0700

    change

--- controllers/user/HomeController.js
+++ controllers/user/HomeController.js
@@ -1,44 +1,6 @@
-const GuestBook = require(`${process.env.ROOT}/models/guestbook`);
-const User = require(`${process.env.ROOT}/models/user`);
-
 class HomeController {
 	index(req,res){
-		console.log("Controller");
-
-		// GuestBook.findAll({	
-			// where : {
-				// id : 1,
-				// '$user.id$' : 1
-			//},						
-			//include : [
-			   // {		
-			   // 		model : User,	   	
-			   // 		as : "user",		       		            
-			   // 		where : {
-			   // 			'id' : 2
-			   // 		},
-		    //         attributes: ['id'],		
-		            // required : false,
-			   		// include : "guest_books",
-			   		// separate : true,
-			   		// order : [
-			   			// ['createdAt','DESC'],
-			   		//]
-		// 	   }
-		// 	]
-		// }).then(data => {
-		// 	console.log(data[0]);
-		// });
-
-		// let limit = 10;
-		// let offset = 0 + (req.body.page - 1) * limit;
-		// Data.findAndCountAll({
-		// offset : offset,
-		// limit : limit,
-		// order : []
-		// }).then(result => {
-        // return res.status(200).json({status : true,message: '',innerData : result})
-		// })
+		console.log("Controller");		
 
 		// res.send("Testing Controller User");
 
--- models/guestbook.js
+++ models/guestbook.js
@@ -1,8 +1,7 @@
 const {Sequelize,DataTypes} = require("sequelize");
-const User = require("./user");
+const DB = require("../config/database.js");
 
-const GuestBook = require("../config/database.js")
-  .define("guest_books",{
+const GuestBook = DB.define("guest_books",{
     userId : {
       type: DataTypes.INTEGER
     },
@@ -14,12 +13,64 @@ const GuestBook = require("../config/database.js")
   }
 );
 
-GuestBook.belongsTo(User,{ 
+GuestBook.belongsTo(DB.define("users"),{ 
 	as : 'user',
 });
 
-GuestBook.Hello = () => {
-  console.log("Hello");
+GuestBook.Testing = () => {
+  console.log("Hello Testing");
 };
 
 module.exports = GuestBook;
+
+/*
+// Reational Database
+const GuestBook = require(`${process.env.ROOT}/models/guestbook`);
+const User = require(`${process.env.ROOT}/models/user`);
+GuestBook.findAll({ 
+      where : {
+        id : 1,
+        // '$user.id$' : 1
+      },            
+      include : [
+         {    
+            model : User,     
+            as : "user",                          
+            // where : {
+              // 'id' : 2
+            // },
+                attributes: ['id'],   
+                // required : false,
+            include : {
+              model : GuestBook,
+              as : "guest_books",
+              limit : 10
+            },
+            // separate : true,
+            // order : [
+              // ['createdAt','DESC'],
+            // ]
+         }
+      ]
+    }).then(data => {
+      console.log(data[0].user);
+    });
+*/
+
+
+/*
+  // Pagination
+
+  let limit = 10;
+  let offset = 0 + (req.body.page - 1) * limit;
+  GuestBook.findAndCountAll({
+     offset : 10,
+     limit : 10,   
+  }).then(result => {
+    return res.status(200).json({
+      status : true,
+      message: '',
+      innerData : result
+      });
+  });
+*/
